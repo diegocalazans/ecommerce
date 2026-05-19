148e66fd3fea6dc356e3f2e128121531bf725ccd
Diego Calazans <diegoalexcalazans@gmail.com>
2026-05-19 09:31:15 -0300

    Ajustes no CRUD

--- src/main/java/br/com/estagio/ecommerce/core/service/PessoaService.java
+++ src/main/java/br/com/estagio/ecommerce/core/service/PessoaService.java
@@ -1,9 +1,12 @@
 package br.com.estagio.ecommerce.core.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import br.com.estagio.ecommerce.core.entity.PessoaEntity;
 import br.com.estagio.ecommerce.core.repository.PessoaRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PessoaService {
@@ -16,6 +19,19 @@ public class PessoaService {
 	
 	public void salvar(PessoaEntity pessoa) {
 		repository.save(pessoa);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public PessoaEntity buscarPorId(Integer id) {				
+		return repository.findById(id)
+				.orElseThrow(EntityNotFoundException::new);
+	}
+	
+	public List<PessoaEntity> buscarTodos() {
+		return repository.findAll();
 	}
 	
 	
--- src/main/java/br/com/estagio/ecommerce/web/controller/PessoaRestController.java
+++ src/main/java/br/com/estagio/ecommerce/web/controller/PessoaRestController.java
@@ -1,6 +1,13 @@
 package br.com.estagio.ecommerce.web.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +29,28 @@ public class PessoaRestController {
 	public void cadastrar(@RequestBody PessoaEntity pessoa) {
 		service.salvar(pessoa);
 	}
+	
+	@PutMapping
+	public void alteracao(@RequestBody PessoaEntity pessoa) {
+		service.salvar(pessoa);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Integer id) {
+		service.delete(id);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PessoaEntity> buscarPeloId(@PathVariable Integer id) {
+		PessoaEntity pessoa = service.buscarPorId(id);
+		return ResponseEntity.ok(pessoa);
+	}
+	
+	@GetMapping("/listar")
+	public ResponseEntity<List<PessoaEntity>> listar() {
+		List<PessoaEntity> pessoas = service.buscarTodos();
+		return ResponseEntity.ok(pessoas);
+	}
+
 
 }
