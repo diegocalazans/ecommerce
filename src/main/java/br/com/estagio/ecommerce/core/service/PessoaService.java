8811bd1099da7c6a4842db0bdaf99a14efe15a4e
Diego Calazans <diegoalexcalazans@gmail.com>
2026-05-25 10:04:34 -0300

    Validacao cpf e exceptions

--- src/main/java/br/com/estagio/ecommerce/core/service/PessoaService.java
+++ src/main/java/br/com/estagio/ecommerce/core/service/PessoaService.java
@@ -2,7 +2,6 @@ package br.com.estagio.ecommerce.core.service;
 
 import java.util.List;
 
-import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -13,37 +12,35 @@ import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PessoaService {
-	
+
 	private final PessoaRepository repository;
 
-	public PessoaService(PessoaRepository repository) {		
+	public PessoaService(PessoaRepository repository) {
 		this.repository = repository;
 	}
-	
+
 	private boolean existe(PessoaEntity pessoa) {
 		Integer id = ObjectUtils.isEmpty(pessoa.getId()) ? 0 : pessoa.getId();
 		return repository.countByCpfAndIdNot(pessoa.getCpf(), id) > 0;
 	}
-	
+
 	public void salvar(PessoaEntity pessoa) {
 		if (existe(pessoa)) {
 			throw new EcommerceException("Já existe pessoa cadastrada com este cpf");
-		}	
+		}
 		repository.save(pessoa);
 	}
-	
+
 	public void delete(Integer id) {
 		repository.deleteById(id);
 	}
-	
-	public PessoaEntity buscarPorId(Integer id) {				
-		return repository.findById(id)
-				.orElseThrow(EntityNotFoundException::new);
+
+	public PessoaEntity buscarPorId(Integer id) {
+		return repository.findById(id).orElseThrow(EntityNotFoundException::new);
 	}
-	
+
 	public List<PessoaEntity> buscarTodos() {
 		return repository.findAll();
 	}
-	
-	
+
 }
